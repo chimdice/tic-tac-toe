8d0b449250891f5b2f531f8c091a31c6d3c7dfa7
chimdice <118031439+chimdice@users.noreply.github.com>
2024-05-28 16:12:02 -0700

    Update Readme and fix board storing past results

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tic-tac-toe
+In this project, I created a tic-tac-toe game. To play, input the name of the two players and players take turns selecting spots on the board to place their element. Once a player wins or games in a draw, please click the reset button to play again.
--- script.js
+++ script.js
@@ -4,31 +4,16 @@ function gameBoard () {
         [0,0,0],
         [0,0,0]
     ];
-    return {board};
-};
 
-
-function player (name, marker) {
-    const {board} =  gameBoard();
-    let numTurns = 0;
-    const addTurn = () => {
-        numTurns++;
-    };
-
-    const displayTurns = () => {
-        return numTurns;
-    };
-
-    const choosePosition = (row, column) => {
+    const choosePosition = (row, column, markerIn) => {
         row = Number(row);
         column = Number(column);
 
-        board[row][column] = marker;
-        console.log(board);
+        board[row][column] = markerIn;
         let win = false;
 
         const checkRowVictory = () => {
-            let numMarkerInRow = board[row].filter((value) => value == marker ).length;
+            let numMarkerInRow = board[row].filter((value) => value == markerIn ).length;
             if (numMarkerInRow == 3) {
                 return true
             };
@@ -38,7 +23,7 @@ function player (name, marker) {
         const checkColumnVictory = () => {
             let numMarkerInCol = 0;
             for (i=0;i<3;i++) {
-                if (board[i][column] == marker) {
+                if (board[i][column] == markerIn) {
                     numMarkerInCol++;
                 };
             };
@@ -51,13 +36,12 @@ function player (name, marker) {
         const checkDiagonalVictory = () => {
             let combined = row + column;
     
-            console.log(combined);
             if (combined%2 == 0) {
     
                 /*check left diagonal*/
                 let numMarkerLeft = 0;
                 for (i=0;i<3;i++) {
-                    if (board[i][i] == marker) {
+                    if (board[i][i] == markerIn) {
                         numMarkerLeft++;
                     };
                 };
@@ -65,7 +49,7 @@ function player (name, marker) {
                 /*check right diagonal*/
                 let numMarkerRight = 0;
                 for (i=0;i<3;i++) {
-                    if (board[i][2-i] == marker) {
+                    if (board[i][2-i] == markerIn) {
                         numMarkerRight++;
                     };
                 };
@@ -102,14 +86,22 @@ function player (name, marker) {
         
     };
 
-    return {name, marker, board, addTurn, displayTurns, choosePosition};
+    return {board, choosePosition};
 };
 
-function playGame(player1, player2) {
-    
+
+function player (name, marker) {
+    return {name, marker};
+};
+
+function playGame(player1, player2, board, choosePosition) {
+
     let turn = true;
     let gameWin = false;
     let turns = 0
+    const resetBtn = document.querySelector("button[type=reset]");
+    const message = document.querySelector("#results-screen");
+
 
     function switchTurn (turn) {
         if (turn) {
@@ -122,16 +114,48 @@ function playGame(player1, player2) {
     function checkWin (gameWin, player) {
         if (gameWin) {
             message.textContent = `${player.name} won! Please press reset to play again.`;
-        
+            board = [
+                [0,0,0],
+                [0,0,0],
+                [0,0,0]
+            ];
+
         } else if (turns == 9) {
             message.textContent = "Game ends in a draw. Please press reset to play again.";
             gameWin = true;
+            board = [
+                [0,0,0],
+                [0,0,0],
+                [0,0,0]
+            ];
+
+            turn = 0;
         };
     };
 
     const squares = document.querySelectorAll("#square");
 
+    resetBtn.addEventListener("click", (event) => {
+        gameOff = true;
+
+        board = [
+            [0,0,0],
+            [0,0,0],
+            [0,0,0]
+        ];
+
+        squares.forEach((square) => {
+            square.textContent = ""
+        });
+
+        player1Name.value = "";
+        player2Name.value = "";
     
+        message.textContent = "Enter names to start playing.";
+    
+        event.preventDefault();
+    });
+
     squares.forEach((square) => {
         square.addEventListener("click", () => {
             square.setAttribute("style", "font-size:50px; display:flex; justify-content:center; align-items:center")
@@ -140,31 +164,17 @@ function playGame(player1, player2) {
                     if (turn) {
                         let rowcol = square.className.split("");
                         square.textContent = player1.marker;
-                        gameWin = player1.choosePosition(rowcol[0], rowcol[1]);
+                        gameWin = choosePosition(rowcol[0], rowcol[1], player1.marker);
                         turns++
                         checkWin(gameWin, player1);
+
                     } else {
                         let rowcol = square.className.split("");
                         square.textContent = player2.marker;
-                        gameWin = player2.choosePosition(rowcol[0], rowcol[1]);
+                        gameWin = choosePosition(rowcol[0], rowcol[1], player2.marker);
                         turns++
                         checkWin(gameWin, player2);
-                    };
-                    
-                    if (gameWin) {
-                        player1.board = [
-                            [0,0,0],
-                            [0,0,0],
-                            [0,0,0]
-                        ];
-                        console.log(player1.board)
 
-                        player2.board = [
-                            [0,0,0],
-                            [0,0,0],
-                            [0,0,0]
-                        ];
-                        console.log(player2.board)
                     };
 
                     turn = switchTurn(turn);
@@ -175,45 +185,36 @@ function playGame(player1, player2) {
     });   
 };
 
-const startBtn = document.querySelector("button[type=start]");
-const resetBtn = document.querySelector("button[type=reset]");
-const message = document.querySelector("#results-screen");
-let gameOff = true;
+function manageGame() {
+    const startBtn = document.querySelector("button[type=submit]");
+    const message = document.querySelector("#results-screen");
+    let {board, choosePosition} =  gameBoard();
 
-startBtn.addEventListener("click", (event) => {
-    if (gameOff) {
-        message.textContent = "Game currently in session.";
+    let gameOff = true;
+
+    startBtn.addEventListener("click", (event) => {
         const player1Name = document.querySelector("#player1");
         const player2Name = document.querySelector("#player2");
-        gameOff = false;
+        if ((player1Name.value.length > 0) && (player2Name.value.length > 0)) {
+            const player1 = player(player1Name.value, "o");
+            const player2 = player(player2Name.value, "x");
 
-        const player1 = player(player1Name.value, "o");
-        const player2 = player(player2Name.value, "x");
-
-        player1Name.value = "";
-        player2Name.value = "";
-
-        playGame(player1, player2);
-
-        event.preventDefault();
-    };
-})
-
-resetBtn.addEventListener("click", (event) => {
-    gameOff = true;
-    const squares = document.querySelectorAll("#square");
-
-    const player1Name = document.querySelector("#player1");
-    player1Name.value = "";
-
-    const player2Name = document.querySelector("#player2");
-    player2Name.value = "";
-
-    squares.forEach((square) => {
-        square.textContent = ""
+            if (gameOff) {
+                message.textContent = "Game currently in session.";
+                gameOff = false;
+        
+                playGame(player1, player2, board, choosePosition);
+        
+                event.preventDefault();
+            };
+        } else {
+            alert("Please input a player(s) name!");
+        }
+        
     });
 
-    message.textContent = "Enter names to start playing.";
+};
 
-    event.preventDefault();
-});
+
+
+manageGame();
